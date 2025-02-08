876334cbc4343f7bd85ab7309fb3b5e6c914e7fb
Aeroo <decastropaulandrew11@gmail.com>
2025-02-08 13:31:22 +0800

    Add project files.

---
+++ MVCSampleProject_DeCastro/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using MVCSampleProject_DeCastro.Models;
+using System.Diagnostics;
+
+namespace MVCSampleProject_DeCastro.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVCSampleProject_DeCastro/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVCSampleProject_DeCastro.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
